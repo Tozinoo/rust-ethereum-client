884da39a67ada5b98b085a88f4ef75c7a2a91aad
Tozinoo <goldboy122@gmail.com>
2024-02-15 18:17:45 +0900

    feat : decrypt keystore file

--- src/accounts/keystore/mod.rs
+++ src/accounts/keystore/mod.rs
@@ -1,2 +1,3 @@
 pub mod keystores;
 pub mod encrypt_private_key;
+pub mod decrypt_keystore;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::accounts::keystore::decrypt_keystore::decrypt_keystore_file;
 use crate::accounts::keystore::keystores::generate_keystore;
 
 mod accounts;
@@ -6,4 +7,5 @@ mod constants;
 
 fn main() {
     let _ = generate_keystore();
+    let _ = decrypt_keystore_file();
 }
